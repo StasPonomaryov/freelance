7151ad7c556df407e82cf2a3d4fb84dc933e66e0
StasPonomaryov <ponomaryov.stas@gmail.com>
2023-10-25 15:37:45 +0300

    added progress bar while navigating

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import { AuthUserProvider } from '../context/AuthUserContext';
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
 
 // TODO: add Clients and Orders contexts
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <AuthUserProvider>
-      <Layout>
+      <NextNProgress />
+      <Layout>        
         <Component {...pageProps} />
       </Layout>
     </AuthUserProvider>
